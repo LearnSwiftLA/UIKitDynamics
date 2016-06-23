0af7d8572a683574d5b2527691671006397b1687
Garric Nahapetian <garricn@icloud.com>
2016-06-22 21:19:13 -0700

    wip. Add dynamic attachment, snap, and push behaviors.

--- UIKitDynamicsLSLA/ViewController.swift
+++ UIKitDynamicsLSLA/ViewController.swift
@@ -9,17 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var greenView: UIView!
+
+    var animator: UIDynamicAnimator!
+    var attachmentBehavior: UIAttachmentBehavior!
+    var snapBehavior: UISnapBehavior!
+    var snapPoint: CGPoint!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        animator = UIDynamicAnimator(referenceView: view)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func handlePanGesture(sender: UIPanGestureRecognizer) {
+        let touchPoint: CGPoint = sender.locationInView(view)
+
+        switch sender.state {
+        case .Began:
+            snapPoint = greenView.center
+            animator.removeAllBehaviors()
+
+            let axis = CGVector(dx: 1, dy: 0)
+            attachmentBehavior = UIAttachmentBehavior.slidingAttachmentWithItem(
+                greenView,
+                attachmentAnchor: touchPoint,
+                axisOfTranslation: axis)
+            animator.addBehavior(attachmentBehavior)
+        case .Changed:
+            attachmentBehavior.anchorPoint = touchPoint
+        case .Ended:
+            animator.removeBehavior(attachmentBehavior)
+
+            let screenHeight = UIScreen.mainScreen().bounds.height
+            let greenViewCenterY = greenView.center.y
+            let withinYThreshold =
+                greenViewCenterY > screenHeight * 0.25 &&
+                greenViewCenterY < screenHeight * 0.75 ? true : false
+
+            if withinYThreshold {
+                let dynamicBehavior = UIDynamicItemBehavior(items: [greenView])
+                dynamicBehavior.allowsRotation = false
+                animator.addBehavior(dynamicBehavior)
+
+                snapBehavior = UISnapBehavior(item: greenView, snapToPoint: snapPoint)
+                snapBehavior.damping = 1.0
+                animator.addBehavior(snapBehavior)
+            } else {
+                let pushBehavior = UIPushBehavior(items: [greenView], mode: .Instantaneous)
+                pushBehavior.pushDirection = CGVector(dx: 0, dy: 1)
+                let velocity = sender.velocityInView(view)
+                pushBehavior.magnitude = sqrt((velocity.x * velocity.x) + (velocity.y + velocity.y))
+                animator.addBehavior(pushBehavior)
+            }
+        default:
+            break
+        }
     }
-
-
 }
 
